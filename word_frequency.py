b9d6ad2fa7ca2b9f2df992b630611e0613a20fdf
Hieu Hoang Duc <hhieu@uoguelph.ca>
2025-10-22 12:38:33 -0400

    Implement sentence validation and word frequency calculation
    Added functions to get a valid sentence, calculate word frequencies, and print them. Included global variables for punctuation handling.

--- word_frequency.py
+++ word_frequency.py
@@ -5,6 +5,53 @@
 
 import re
 
+#Global variable
+end_punctuations = ['.', '?', '!']
+punctuations = ['(',')',',','.','!','?',"'",'"', ':', ';']
+
+#Get sentence function
+def get_sentence()
+    sentence = input("Enter a sentence")
+    while (not sentence[0].isupper()) or (sentence[-1] not in end_punctuations) or (len(sentence.strip()) == 0):
+        sentence = input("Please enter a valid sentence")
+    return sentence
+    
+#Calculate frequency function
+def calculate_frequencies(sentence):
+    for char in punctuations:
+        sentence = sentence.replace(char, "")
+    #Split the sentence
+    split_words = sentence.split()
+    #Initialize word_list and frequency 
+    word_list = []
+    frequency = []
+    #Loop
+    for word in split_words:
+        word = word.lower()
+        if word in word_list:
+            frequency_index = word_list.index(word)
+            frequency_index[frequency_index] += 1
+        else:
+            word_list.append(word)
+            frequency.append(1)
+    return word_list, frequency
+
+#Print function
+def print_frequencies(words, frequencies):
+    print("Frequencies of Words is:")
+    for i in range(len(words)):
+        print(f"{words[i]}: {frequencies[i]}")
+
+#Main function
+def main():
+    sentence = get_sentence()
+    words, frequencies = calculate_frequencies(sentence)
+    print_frequencies(words, frequencies)
+
+#Run the program
+main()
+
+
 #This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
 def is_sentence(text):
     # Check if the text is not empty and is a string
